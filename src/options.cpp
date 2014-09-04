92336b40b24aa3de50172dd06a99f5900bf4a36e
Daniel Dörrhöfer <daniel@luke>
2014-09-04 19:45:44 +0200

    This class has been extended to cover all settings in properties dialog.

--- src/options.cpp
+++ src/options.cpp
@@ -1,10 +1,16 @@
 #include "options.h"
 
 static const char* ADDROOTPATH_ENABLED = "properties/rootpathtofilename";
+static const char* DEFAULTHASH = "properties/defaulthash";
+static const char* ROOTPATHTYP = "properties/rootpathtyp";
+static const char* STLYE = "properties/style";
 
 Options::Options(QObject *parent) :
     QObject(parent),
-     m_addRootPath(true)
+    m_addRootPath(true),
+    m_defaultHash("SHA256"),
+    m_rootPathTyp("dynamic"),
+    m_style("MD5")
 {
 }
 
@@ -12,12 +18,70 @@ void Options::readSettings()
 {
      QSettings settings;
 
+     //settings.remove("properties");
+
      m_addRootPath = settings.value(ADDROOTPATH_ENABLED, true).toBool();
-}
+     m_defaultHash = settings.value(DEFAULTHASH, "SHA256").toString();
+     m_rootPathTyp = settings.value(ROOTPATHTYP, "dynamic").toString();
+     m_style = settings.value(STLYE, "MD5").toString();
+ }
 
 void Options::writeSettings()
 {
     QSettings settings;
 
     settings.setValue(ADDROOTPATH_ENABLED, m_addRootPath);
+    settings.setValue(DEFAULTHASH, m_defaultHash);
+    settings.setValue(ROOTPATHTYP, m_rootPathTyp);
+    settings.setValue(STLYE, m_style);
 }
+
+bool Options::addRootPath() const
+{
+    return m_addRootPath;
+}
+
+void Options::setAddRootPath(bool addRootPath)
+{
+    m_addRootPath = addRootPath;
+}
+
+QString Options::defaultHash() const
+{
+    return m_defaultHash;
+}
+
+void Options::setDefaultHash(const QString &defaultHash)
+{
+    m_defaultHash = defaultHash;
+}
+
+QString Options::rootPathTyp() const
+{
+    return m_rootPathTyp;
+}
+
+void Options::setRootPathTyp(const QString &rootPathTyp)
+{
+    m_rootPathTyp = rootPathTyp;
+}
+
+QString Options::style() const
+{
+    return m_style;
+}
+
+void Options::setStyle(const QString &style)
+{
+    m_style = style;
+}
+
+void Options::apply()
+{
+    emit initProperties();
+    emit init_hashtyp();
+}
+
+
+
+
--- src/options.h
+++ src/options.h
@@ -12,13 +12,27 @@ public:
     void readSettings();
     void writeSettings();
 
+    bool addRootPath() const;
+    void setAddRootPath(bool addRootPath);
+    QString defaultHash() const;
+    void setDefaultHash(const QString &defaultHash);
+    QString rootPathTyp() const;
+    void setRootPathTyp(const QString &rootPathTyp);
+    QString style() const;
+    void setStyle(const QString &style);
+    void apply();
+
 signals:
+    void initProperties();
+    void init_hashtyp();
 
 public slots:
 
 private:
     bool m_addRootPath;
-
+    QString m_defaultHash;
+    QString m_rootPathTyp;
+    QString m_style;
 
 };
 
